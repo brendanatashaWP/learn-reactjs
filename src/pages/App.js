194243cbff9376a9ccb9f6f3987995d77a22f557
BRENDA-ACER\Brenda <brnjdb@gmail.com>
2018-09-23 11:16:50 +0700

    Add state

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loading: true
+    }
+  }
   render() {
     return (
-      <h1>Hello world from brenda</h1>
+      <h1>loading: {JSON.stringify(this.state.loading)}</h1>
     )
   }
 }
