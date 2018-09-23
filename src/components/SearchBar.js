21b2635cf1e864f02f2863eaa724189129e7fb24
BRENDA-ACER\Brenda <brnjdb@gmail.com>
2018-09-23 14:48:26 +0700

    Add searchbar styles

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,15 +2,29 @@ import React from 'react'
 
 const SearchBar = props  => {
     return (
-        <div>
-            <input 
+        <div style={styles.contentSearch}>
+            <input
+                style = {styles.inputSearch}
                 type="text" 
                 placeholder="search by title"
-                value={props.search}
                 onChange={props.onChangeSearch}
             />
         </div>
     )
+}
+
+const styles = {
+    inputSearch: {
+      width: 500,
+      height: 30
+    },
+    contentSearch: {
+        backgroundColor: "#585481",
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex"
+    }
 }
 
 export default SearchBar
